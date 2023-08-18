0b418b3a20542148c93eb3c02d823b4eac406bc7
Beaufaye BAOUROU <baouroubeaufaye881@gmail.com>
2023-08-18 14:48:26 +0200

    mis à jour

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer class="foot ignore bg-danger">
+    <h3 class="text-light">Copyright@ GETUD 2023 </h3>
+</footer>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+require_once ("connexion.php");
+  include("class/compte.class.php");
+  $bd= bd();
+  $compte =new compte($bd);
+if (isset($_POST["username"]) AND isset($_POST["password"])) {
+    
+    $user=$compte->authenticate($_POST["username"],$_POST["password"]);
+    if ($user!=false) {
+        session_start();
+        $_SESSION["user"]=$user[0];
+        header("location:acceuil.php");
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8">
+      <title>connexion</title>
+      <link rel="icon" href="picture/simplonco ulule.png" type="image">
+      <link rel="stylesheet" media="screen" href="font/css/login.css">
+      <link rel="stylesheet" href="font/css/font-awesome.min.css">
+      <link rel="stylesheet" href="font/css/bootstrap.min.css">
+      <link rel="stylesheet" href="font/css/style.css">
+    </head>
+   <body>
+           
+            
+            <div class="right  bg-white">
+                <h3 class="auth m-3">AUTHENTIFICATION</h3>
+                <div class="d-flex auth">
+                 <img src="font/images/logo univ.jpg" alt="image lest" class="ent-img">
+                </div>
+            <form class=" form p-4 d-flex flex-column w-100" action="" method="POST" enctype="multipart/form-data">
+                <label for="">Nom d'utilisateur</label>      
+                <input type="text" class="champ" name="username" required>
+                <label for="">Mot de passe</label>   
+                <input type="password" class="champ" name="password"> 
+                <div class="panel-footer mt-3">
+                    <button type="submit" class="btn btn-danger pull-left mr-3"> Se Connecter </button>
+                </div>
+            </form>
+            </div>
+            <div class="left bg-danger">
+                 <h2 class="text-light"> BIENVENUE DANS GETUD </h2>
+                 <h3 class="text-warning"> Une plateforme de Gestion des étudiants de l'Université Thomas SANKARA </h3>
+            </div>
+   </body>
+   
+</html>
