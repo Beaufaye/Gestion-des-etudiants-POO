6ad4e1aa546c247f56a5dafddf11a98466dca030
Beaufaye BAOUROU <baouroubeaufaye881@gmail.com>
2023-08-18 16:35:44 +0200

    mis à jour

--- bd/bd2.sql
+++ bd/bd2.sql
@@ -1,27 +1,16 @@
 /*==============================================================*/
 /* Nom de SGBD :  MySQL 5.0                                     */
-/* Date de crï¿½ation :  18/08/2023 14:50:43                      */
+/* Date de création :  18/08/2023 16:11:36                      */
 /*==============================================================*/
 
-
-drop table if exists compte;
 
 drop table if exists etudiant;
 
-drop table if exists role;
+drop table if exists filiere;
+
+drop table if exists niveau_etude;
 
 drop table if exists utilisateur;
-
-/*==============================================================*/
-/* Table : compte                                               */
-/*==============================================================*/
-create table compte
-(
-   id_compte            int not null,
-   username             varchar(254),
-   password             varchar(254),
-   primary key (id_compte)
-);
 
 /*==============================================================*/
 /* Table : etudiant                                             */
@@ -29,27 +18,36 @@ create table compte
 create table etudiant
 (
    id_etudiant          int not null,
+   id_niveau            int not null,
+   id_filiere           int not null,
    nom                  varchar(254),
    prenom               varchar(254),
    date_de_naissance    datetime,
    nation               varchar(254),
-   filiere              varchar(254),
-   niveau_d_etude       varchar(254),
    adresse              varchar(254),
-   email                varchar(254),
    contact              int,
-   primary key (id_etudiant)
+   primary key (id_etudiant),
+   key AK_Identifiant_2 (id_etudiant)
 );
 
 /*==============================================================*/
-/* Table : role                                                 */
+/* Table : filiere                                              */
 /*==============================================================*/
-create table role
+create table filiere
 (
-   id_role              int not null,
-   id_utilisateur       int not null,
-   type_de_role         varchar(254),
-   primary key (id_role)
+   id_filiere           int not null,
+   nom_filiere          varchar(254),
+   primary key (id_filiere)
+);
+
+/*==============================================================*/
+/* Table : niveau_etude                                         */
+/*==============================================================*/
+create table niveau_etude
+(
+   id_niveau            int not null,
+   valeur               int,
+   primary key (id_niveau)
 );
 
 /*==============================================================*/
@@ -58,18 +56,18 @@ create table role
 create table utilisateur
 (
    id_utilisateur       int not null,
-   id_compte            int not null,
    nom                  varchar(254),
    prenom               varchar(254),
-   adresse              varchar(254),
-   email                varchar(254),
+   uersname             varchar(254),
+   password             varchar(254),
    contact              int,
-   primary key (id_utilisateur)
+   primary key (id_utilisateur),
+   key AK_Identifiant_2 (id_utilisateur)
 );
 
-alter table role add constraint FK_Association_2 foreign key (id_utilisateur)
-      references utilisateur (id_utilisateur) on delete restrict on update restrict;
+alter table etudiant add constraint FK_Association_1 foreign key (id_filiere)
+      references filiere (id_filiere) on delete restrict on update restrict;
 
-alter table utilisateur add constraint FK_Association_4 foreign key (id_compte)
-      references compte (id_compte) on delete restrict on update restrict;
+alter table etudiant add constraint FK_Association_2 foreign key (id_niveau)
+      references niveau_etude (id_niveau) on delete restrict on update restrict;
 
